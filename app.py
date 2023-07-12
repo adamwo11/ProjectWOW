ad95f73e3fddfbd4315260c9fa4bb83f53ecef3a
Adamwo11 <adamwo@adam.com.au>
2023-07-12 13:39:59 +0930

    render WOW

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, redirect
+from flask import Flask, redirect, send_from_directory
 from routes.char_routes import char_routes
 from routes.users_routes import users_routes
 from routes.sessions_routes import sessions_routes
@@ -15,4 +15,12 @@ app.register_blueprint(sessions_routes, url_prefix='/sessions')
 
 @app.route('/')
 def index():
-  return redirect('/char')
+    return redirect('/char')
+
+# @app.route('/images/<path:filename>')
+# def serve_image(filename):
+#     root_dir = os.path.dirname(os.getcwd())
+#     return send_from_directory(os.path.join(root_dir, 'templates', 'images'), filename)
+
+if __name__ == '__main__':
+    app.run()
--- db/schema.sql
+++ db/schema.sql
@@ -31,7 +31,7 @@ SELECT * FROM char;
 
 INSERT INTO char(name, realm, level, img_url, gender, classes, race, faction)
 VALUES
-('Plumbob', 'Frostmourne', '70','https://scontent.fadl3-1.fna.fbcdn.net/v/t1.15752-9/346154294_1871539633213919_1365357872945219264_n.png?stp=dst-png_p320x320&_nc_cat=100&ccb=1-7&_nc_sid=aee45a&_nc_ohc=DeK5_ZYC7EoAX9l_0yy&_nc_ht=scontent.fadl3-1.fna&oh=03_AdQ2aLKZttt-eAApCPPCXPr4DllpyE9deX7mbcLMiRbPTQ&oe=6489074A', 'Male', 'Hunter', 'Nightelf', 'Alliance'),
+('Plumbob', 'Frostmourne', '70','../templates/images/Hunter.png', 'Male', 'Hunter', 'Nightelf', 'Alliance'),
 ('Warwiz', 'Frostmourne', '56', 'https://scontent.fadl3-1.fna.fbcdn.net/v/t1.15752-9/346136188_162589140114245_5896913629695510506_n.png?stp=dst-png_s403x403&_nc_cat=104&ccb=1-7&_nc_sid=aee45a&_nc_ohc=6TZpTabnPxEAX8BAABv&_nc_ht=scontent.fadl3-1.fna&oh=03_AdRL2c4GtlHcRVBgdNRMWOhHqoe7F1PZQbh-ORact_EzEw&oe=6488FDCE', 'Male', 'Warlock', 'Orc', 'Horde');
 
 
